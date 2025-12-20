744f9067dc80a86d70fb26f6a4b7dd59525dbc49
Dipjyoti <nathdipjyoti844@gmail.com>
2025-12-20 18:22:53 +0530

    axios api fix

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.REACT_APP_API_URL ||
+  "http://localhost:5000";
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL,
   timeout: 30000,
 });
 
